d76a27c13f09cc2ea81c938c01f13b91da46c564
geokeloglou <geokeloglou@gmail.com>
2021-01-06 20:39:20 +0200

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,11 +1,15 @@
 <?php
 
-// dev
-$mysqli_connection = mysqli_connect('localhost', 'root', 'root', 'adise20_avissinia'); 
-if ($mysqli_connection->connect_error) {
-    echo "Not connected, error: " . $mysqli_connection->connect_error;
+// $mysqli_connection = mysqli_connect('localhost', 'root', 'root', 'adise20_avissinia'); 
+
+if (gethostname()=='users.iee.ihu.gr') {
+  $mysqli_connection = new mysqli('localhost', 'root', 'root', 'adise20_avissinia', null, '/home/staff/it154466/mysql/run/mysql.sock');
+} else {
+  $mysqli_connection = new mysqli('localhost', 'root', 'root', 'adise20_avissinia');
 }
 
-// prod
+if ($mysqli_connection->connect_errno) {
+  echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
 
 ?>
